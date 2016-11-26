e64350ab170039b064303e374aa097bafa6c4edd
Jarosław Skrzypczak <jarek.skrzypczak@o2.pl>
2016-11-26 23:32:11 +0100

    Dodano nową pętlę

--- src/main.java
+++ src/main.java
@@ -3,6 +3,20 @@
  */
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println(Integer.toString(addition(1,2)));
+        System.out.println("Hello");
+        for (int i= 0; i<args.length; i++){
+            System.out.println(args[i]);
+        }
+    }
+
+    /**
+     * Dodawnie x+y
+     * @param x
+     * @param y
+     * @return Sum
+     */
+    public static int addition(int x, int y){
+        return x+y;
     }
 }
