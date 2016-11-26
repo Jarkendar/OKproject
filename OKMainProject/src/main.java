cba18f2b557b36ac1146993f6327b86d56c092ea
Jarosław Skrzypczak <jarek.skrzypczak@o2.pl>
2016-11-26 23:43:14 +0100

    Working files

--- OKMainProject/src/main.java
+++ OKMainProject/src/main.java
@@ -5,17 +5,12 @@ public class main {
     public static void main(String[] args) {
         System.out.println(Integer.toString(addition(1,2)));
         System.out.println("Hello");
-        for (int i= 0; i<args.length; i++){
-            System.out.println(args[i]);
+        for(i:args){
+            System.out.println(i);
         }
     }
 
-    /**
-     * Dodawnie x+y
-     * @param x
-     * @param y
-     * @return Sum
-     */
+
     public static int addition(int x, int y){
         return x+y;
     }
