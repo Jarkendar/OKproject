d39b2d8373f91b20f8a5c6cba2ab247d3feab880
Jarosław Skrzypczak <jarek.skrzypczak@o2.pl>
2016-12-03 20:56:29 +0100

    Dokończenie pisania, rozpoczęcie testów.

--- OKMainProject/src/GeneratorPackage/MainGenerator.java
+++ OKMainProject/src/GeneratorPackage/MainGenerator.java
@@ -23,19 +23,50 @@ public class MainGenerator {
         Task[] tasks = new Task[size];
         Maintanance[] maintanances = new Maintanance[maintanance_count];
 
-        fillTasksArray(tasks, size, max_task_time);
-        fillMaintananceArray(maintanances,machine_count);
+        all_time = fillTasksArray(tasks, size, max_task_time);
+        fillMaintananceArray(maintanances,machine_count, max_task_time, all_time);
 
+        DisplayTasks(tasks);
+        DisplayMaintanance(maintanances);
     }
 
-    private static void fillMaintananceArray(Maintanance[] maintanances, int size){
-        Random random = new Random(System.currentTimeMillis());
-        for (int i =0 ; i<size; i++){
-
+    private static void DisplayMaintanance(Maintanance[] maintanances){
+        for (int i = 0; i<maintanances.length; i++){
+            System.out.println(i + ";" + maintanances[i].getNr_machine() + ";"
+                                + maintanances[i].getTime() + ";"
+                                + maintanances[i].getTime_start() + ";");
         }
     }
 
-    private static void fillTasksArray(Task[] tasks, int size, int max_task_time){
+    private static void DisplayTasks(Task[] tasks){
+        for (Task x: tasks) {
+            System.out.println(x.getTime_part1() + ";"
+                            + x.getTime_part2() + ";"
+                            + x.getNr_machine_part1() + ";"
+                            + x.getNr_machine_part2() + ";"
+                            + x.getReady_time_part1() + ";");
+        }
+    }
+
+    private static void fillMaintananceArray(Maintanance[] maintanances, int size, int max_time, int all_time){
+        Random random = new Random(System.currentTimeMillis());
+        for (int i =0 ; i<size; i++){
+            boolean machine = false;
+            int time, start_time, nr_mach;
+            machine = random.nextBoolean();
+            if (machine){
+                nr_mach = 1;
+            }else {
+                nr_mach = 2;
+            }
+            time = random.nextInt(max_time);
+            start_time = random.nextInt(all_time/4);
+            Maintanance maintanance = new Maintanance(nr_mach,time,start_time);
+            maintanances[i] = maintanance;
+        }
+    }
+
+    private static int fillTasksArray(Task[] tasks, int size, int max_task_time){
         int all_time = 0;
         Random random = new Random(System.currentTimeMillis());
         for(int i =0; i<size; i++){
@@ -57,7 +88,7 @@ public class MainGenerator {
             Task task = new Task(time1,time2,nr_mac1, nr_mac2, rdy_time1);
             tasks[i] = task;
         }
-
+        return all_time;
     }
 
 }
