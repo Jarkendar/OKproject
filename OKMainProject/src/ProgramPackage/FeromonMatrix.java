d4265193e4778795997016b311526e8638fc2ee3
Jarosław Skrzypczak <jarek.skrzypczak@o2.pl>
2016-12-05 16:57:51 +0100

    Dodanie szkieletu macierzy feromonowej, z przykładowymi funkcjami bez implementacji.

---
+++ OKMainProject/src/ProgramPackage/FeromonMatrix.java
@@ -0,0 +1,53 @@
+package ProgramPackage;
+
+/**
+ * Created by Jarek on 2016-12-05.
+ */
+public class FeromonMatrix {
+    private double[][] feromonMatrix;
+    //pionowo nr zadania od którego idziemy - from
+    //poziomo nr zadania do którego idziemy - to
+    //            to
+    //    from |
+
+    public double[][] getFeromonMatrix() {
+        return feromonMatrix;
+    }
+
+    public void setFeromonMatrix(double[][] feromonMatrix) {
+        this.feromonMatrix = feromonMatrix;
+    }
+
+
+    /**
+     * Konstruktor macierzy
+     * @param size - ilość zadań
+     */
+    public FeromonMatrix(int size) {
+        this.feromonMatrix = new double[size][size];
+    }
+
+    public void alignValueInMatrix(){
+        //zmniejsza różnice między najbardziej i najmniej wyczuwalnym śladem feromonowym
+    }
+
+    /**
+     * Dodaje trochę feromonu na ścieżce pomiędzy zadaniami from->to
+     * @param from - od nr zadania
+     * @param to - do nr zdania
+     */
+    public void addFeromonToWay(int from, int to){
+        feromonMatrix[to][from] += 1; //wartość do ustalenie
+    }
+
+    public int[] useFeromonMatrix(){
+        //użycie macierzy feromonowej
+    }
+
+    private int ruleteAlgorithm(int from){
+        //wylosowanie docelowej ścieżki na podstawie algorytmu ruletki
+    }
+
+
+
+}
